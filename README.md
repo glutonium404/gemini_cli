cb7dc7403895607ddced7045ca8e22cc72b12676
glutonium69 <tahzib404@gmail.com>
2026-03-30 22:44:59 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+first make sure to have gemini api key in you bashrc
+
+```sh
+GEMINI_API_KEY='<your api key>'
+```
+
+to convert to binary using node
+
+```sh
+# Install a fast bundler temporarily
+npx esbuild index.js --bundle --platform=node --outfile=dist/bundle.js
+
+# This downloads pkg temporarily and compiles your bundle
+npx pkg dist/bundle.js --target node18-linux-static-x64 --output gemini_cli
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const instruction = "keep the ans short and concise withing 3 to 5 lines. do not format the text. just use plain text. just ans the question, do not add extra text, do as told and keep it at that only. Here is my question: \n\n";
+
+const question = process.argv.slice(2).join(" ");
+
+if (!question) {
+    console.log("Usage: gemini <your question>");
+    process.exit(1);
+}
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({
+    model: "gemini-3.1-flash-lite-preview"
+});
+
+async function ask() {
+    try {
+        const result = await model.generateContent(instruction + question);
+        console.log(result.response.text());
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+ask();
